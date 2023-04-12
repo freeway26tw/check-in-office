024a3ffce481cdd2834b1ad28f7bd98b940255ac
freeway26tw <freeway26tw@gmail.com>
2023-04-13 02:00:39 +0800

    fix signin layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const userController = require('../controllers/user-controller')
 
-router.get('/test', (req, res)=> res.send('hello'))
 router.get('/signin', userController.signInPage)
 
 module.exports = router
