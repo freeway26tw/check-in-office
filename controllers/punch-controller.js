b9f19583db2e56bdf634c3e04f73ab16babd0dad
freeway26tw <freeway26tw@gmail.com>
2023-04-14 17:29:37 +0800

    check taiwan offwork day

--- controllers/punch-controller.js
+++ controllers/punch-controller.js
@@ -1,3 +1,10 @@
+const axios = require('axios')
+
+// axios.get('https://cdn.jsdelivr.net/gh/ruyut/TaiwanCalendar/data/2023.json')
+//   .then(function (response) {
+//     console.log(response.data.filter(d => d.isHoliday === true))
+//   })
+
 const punchController = {
   getDashboard: (req, res) => {
     res.render('dashboard')
